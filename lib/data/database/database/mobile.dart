3b9e2bcfd0b5fbfc9efa06940ddf2431d3972472
Rody Davis <rody.davis.jr@gmail.com>
2019-09-11 08:07:31 -0400

    desktop construct

--- lib/data/database/database/mobile.dart
+++ lib/data/database/database/mobile.dart
@@ -1,8 +1,23 @@
+import 'dart:io';
+
 import 'package:moor_flutter/moor_flutter.dart';
+import 'package:moor/moor_vm.dart';
 
 import '../database.dart';
 
 Database constructDb({bool logStatements = false}) {
-  return Database(FlutterQueryExecutor.inDatabaseFolder(
-      path: 'db.sqlite', logStatements: logStatements));
+  if (Platform.isIOS || Platform.isAndroid) {
+    return Database(FlutterQueryExecutor.inDatabaseFolder(
+        path: 'db.sqlite', logStatements: logStatements));
+  }
+  if (Platform.isMacOS || Platform.isLinux) {
+    final _path = Directory('${Platform.environment['HOME']}/.config');
+    final _file = File('$_path.sqlite');
+    return Database(VMDatabase(_file, logStatements: logStatements));
+  } else if (Platform.isWindows) {
+    final _path = Directory('${Platform.environment['UserProfile']}\\.config');
+    final _file = File('$_path.sqlite');
+    return Database(VMDatabase(_file, logStatements: logStatements));
+  }
+  return Database(VMDatabase.memory(logStatements: logStatements));
 }
